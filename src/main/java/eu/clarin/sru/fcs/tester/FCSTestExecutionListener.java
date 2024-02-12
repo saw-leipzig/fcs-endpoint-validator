5f9d30c7549926c0839c28a3d24deb8fb58e2495
Erik Körner <koerner@saw-leipzig.de>
2024-02-12 14:46:16 +0100

    Improve log capturing (no duplicate output in console)

--- src/main/java/eu/clarin/sru/fcs/tester/FCSTestExecutionListener.java
+++ src/main/java/eu/clarin/sru/fcs/tester/FCSTestExecutionListener.java
@@ -5,7 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.core.LogEvent;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.AppenderRef;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
 import org.apache.logging.log4j.core.layout.PatternLayout;
 import org.junit.platform.engine.TestDescriptor.Type;
 import org.junit.platform.engine.TestExecutionResult;
@@ -18,7 +24,8 @@ import org.slf4j.LoggerFactory;
 public class FCSTestExecutionListener implements TestExecutionListener {
     protected static final Logger logger = LoggerFactory.getLogger(FCSTestExecutionListener.class);
 
-    protected static String LOGCAPTURING_APPENDER_NAME = LogCapturingAppender.class.getName();
+    protected static final String LOGCAPTURING_APPENDER_NAME = LogCapturingAppender.class.getName();
+    protected static final String LOGCAPTURING_LOGGER_NAME = "eu.clarin.sru";
 
     protected LogCapturingAppender appender;
     protected HttpRequestResponseRecordingInterceptor httpRequestResponseRecordingInterceptor;
@@ -73,15 +80,28 @@ public class FCSTestExecutionListener implements TestExecutionListener {
     @Override
     public void testPlanExecutionStarted(TestPlan testPlan) {
         logger.debug("Add log capturing appender");
-        org.apache.logging.log4j.core.Logger sruLogger = ((org.apache.logging.log4j.core.Logger) org.apache.logging.log4j.LogManager
-                .getLogger("eu.clarin.sru"));
-        sruLogger.setAdditive(true);
-        // sruLogger.setLevel(Level.ALL);
 
-        appender = new LogCapturingAppender(LOGCAPTURING_APPENDER_NAME, null,
-                PatternLayout.createDefaultLayout(), true, null);
+        // see
+        // https://logging.apache.org/log4j/2.x/manual/customconfig.html#programmatically-modifying-the-current-configuration-after-initi
+        final LoggerContext ctx = (LoggerContext) LogManager.getContext(false);
+        final Configuration config = ctx.getConfiguration();
+        final PatternLayout layout = PatternLayout.createDefaultLayout(config);
+
+        appender = new LogCapturingAppender(LOGCAPTURING_APPENDER_NAME, null, layout, true, null);
         appender.start();
-        sruLogger.addAppender(appender);
+
+        config.addAppender(appender);
+
+        // add custom appender to get logs
+        AppenderRef ref = AppenderRef.createAppenderRef(LOGCAPTURING_APPENDER_NAME, null, null);
+        AppenderRef[] refs = new AppenderRef[] { ref };
+        @SuppressWarnings("deprecation")
+        LoggerConfig loggerConfig = LoggerConfig.createLogger(false, Level.ALL, LOGCAPTURING_LOGGER_NAME, "true", refs,
+                null, config, null);
+        loggerConfig.addAppender(appender, null, null);
+        config.addLogger(LOGCAPTURING_LOGGER_NAME, loggerConfig);
+
+        ctx.updateLoggers();
     }
 
     @Override
